0a0b50a44661b6d6de852d460b60707d0a5737cb
Joao Moreira <jfmoreir@thoughtworks.com>
2018-05-26 13:38:55 -0300

    Improving code organization

--- src/main/java/com/javaee/emailsender/EmailConfig.java
+++ src/main/java/com/javaee/emailsender/EmailConfig.java
@@ -6,7 +6,10 @@ import java.io.InputStream;
 import java.util.Date;
 import java.util.Properties;
 
+import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -14,7 +17,7 @@ import javax.mail.internet.MimeMessage;
 
 public class EmailConfig {
 
-	public Properties getProperties() throws IOException {
+	private Properties getProperties() throws IOException {
 		InputStream inputStream = null;
 		try {
 			Properties properties = new Properties();
@@ -37,28 +40,53 @@ public class EmailConfig {
 		return null;
 	}
 	
-	public void sendEmail(Session session, String toEmail, String subject, String body){
+	public void sendEmail(final String fromEmail,
+						  final String password,
+						  String toEmail,
+						  String subject,
+						  String body){
 		try {
-	      MimeMessage message = new MimeMessage(session);
-	      message.addHeader("Content-type", "text/HTML; charset=UTF-8");
-	      message.addHeader("format", "flowed");
-	      message.addHeader("Content-Transfer-Encoding", "8bit");
-
-	      message.setFrom(new InternetAddress("example@gmail.com", "Example"));
-	      message.setReplyTo(InternetAddress.parse("example@gmail.com", false));
-	      message.setSubject(subject, "UTF-8");
-	      message.setText(body, "UTF-8");
-	      message.setSentDate(new Date());
-	      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
-	      
-	      System.out.println("Message ready!");
-	      
-    	  Transport.send(message);
-
-	      System.out.println("Email sent successfully!!");
+			Authenticator auth = configAuth(fromEmail, password);
+			
+			Session session = Session.getInstance(getProperties(), auth);
+			
+			MimeMessage message = new MimeMessage(session);
+			configHeaders(message);
+			configContent(toEmail, subject, body, message);
+			System.out.println("Message ready!");
+			
+			Transport.send(message);
+			System.out.println("Email sent successfully!!");
+	    } catch (Exception e) {
+	    	e.printStackTrace();
 	    }
-	    catch (Exception e) {
-	      e.printStackTrace();
-	    }
+	}
+
+	private void configContent(String toEmail, String subject, String body, MimeMessage message) {
+		try {
+			message.setFrom(new InternetAddress("example@gmail.com", "Example"));
+			message.setReplyTo(InternetAddress.parse("example@gmail.com", false));
+			message.setSubject(subject, "UTF-8");
+			message.setText(body, "UTF-8");
+			message.setSentDate(new Date());
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void configHeaders(MimeMessage message) throws MessagingException {
+		message.addHeader("Content-type", "text/HTML; charset=UTF-8");
+		message.addHeader("format", "flowed");
+		message.addHeader("Content-Transfer-Encoding", "8bit");
+	}
+
+	private Authenticator configAuth(final String fromEmail, final String password) {
+		Authenticator auth = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromEmail, password);
+			}
+		};
+		return auth;
 	}
 }
--- src/main/java/com/javaee/emailsender/EmailSender.java
+++ src/main/java/com/javaee/emailsender/EmailSender.java
@@ -1,31 +1,17 @@
 package com.javaee.emailsender;
 
-import java.io.IOException;
-
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-
 public class EmailSender {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		final String fromEmail = "example@gmail.com";
 		final String password = "*********";
 		final String toEmail = "example@gmail.com";
 		
 		System.out.println("Initializing email send");
 		
-		Authenticator auth = new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(fromEmail, password);
-			}
-		};
-		
 		EmailConfig config = new EmailConfig();
 		
-		Session session = Session.getInstance(config.getProperties(), auth);
-		
-		config.sendEmail(session, toEmail, "Subject", "Email Body");
+		config.sendEmail(fromEmail, password, toEmail, "Subject", "Email Body");
 	}
 
 }
